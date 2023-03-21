5f94e64fc0adeee8ce2973aefafaa127e607968f
Justin Louise Ferdes <202010981@gordoncollege.edu.ph>
2023-03-21 11:55:56 +0800

    start

---
+++ backend/config/Connection.php
@@ -0,0 +1,28 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE, PATCH");
+
+date_default_timezone_set("Asia/Manila");
+set_time_limit(1000);
+
+define("SERVER", "localhost");
+define("DBASE", "rtg");
+define("USER", "root");
+define("PASSWORD", "");
+
+class Connection{
+    protected $con_string = "mysql:host=".SERVER.";dbname=".DBASE."; charset=utf8mb4";
+    protected $options = [
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES => false
+    ];
+
+    public function connect(){
+        return new \PDO($this->con_string, USER, PASSWORD, $this->options);
+    }
+}
