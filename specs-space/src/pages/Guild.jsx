511c22d011f723544da0973a980c124168f609c4
JoshuaAyran <202011015@gordoncollege.edu.ph>
2023-04-05 18:36:15 +0800

    user activity page

--- specs-space/src/pages/Guild.jsx
+++ specs-space/src/pages/Guild.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './pagestyle.scss';
 import Sidebar from '../components/Sidebar'
 import { GiTrashCan } from "react-icons/gi";
 import { MdOutlineCheckCircleOutline } from "react-icons/md";
+import { BsChevronExpand } from 'react-icons/bs'
+
+
+const submittedact = [
+  {
+    title: "Do Homework",
+    file: "Submitted-activity.pdf"
+  },
+  {
+    title: "Do Homework",
+    file: "Submitted-activity.pdf"
+  },
+  {
+    title: "Do Homework",
+    file: "Submitted-activity.pdf"
+  },
+]
 
 const quest = [
 
@@ -10,33 +27,104 @@ const quest = [
     questID: "1",
     title: "Do Homework",
     deadline: "3/31/2023",
-    description: "Do Software Engineering Module 1",
+    description: "Minim excepteur mollit laborum dolore sit excepteur consequat. Reprehenderit non reprehenderit occaecat culpa aliquip amet. Non duis voluptate quis ex. Dolore quis labore quis sunt sunt magna dolor commodo est nisi sint sint commodo. Pariatur enim ea magna ad exercitation pariatur dolor aute pariatur. Reprehenderit mollit quis cupidatat in consequat officia voluptate fugiat. Nostrud incididunt deserunt velit anim nostrud eiusmod est minim amet nisi veniam.",
     exp: 100,
+    pts: 50,
   },
   {
     questID: "2",
     title: "Do Homework",
     deadline: "3/31/2023",
-    description: "Do Software Engineering Module 1",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
     exp: 100,
+    pts: 50,
   },
   {
     questID: "3",
     title: "Do Homework",
     deadline: "3/31/2023",
-    description: "Do Software Engineering Module 1",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
     exp: 100,
+    pts: 50,
   },
   {
-    questID: "1",
+    questID: "4",
     title: "Do Homework",
     deadline: "3/31/2023",
-    description: "Do Software Engineering Module 1",
+    description: "Minim excepteur mollit laborum dolore sit excepteur consequat. Reprehenderit non reprehenderit occaecat culpa aliquip amet. Non duis voluptate quis ex. Dolore quis labore quis sunt sunt magna dolor commodo est nisi sint sint commodo. Pariatur enim ea magna ad exercitation pariatur dolor aute pariatur. Reprehenderit mollit quis cupidatat in consequat officia voluptate fugiat. Nostrud incididunt deserunt velit anim nostrud eiusmod est minim amet nisi veniam.",
     exp: 100,
+    pts: 50,
+  },
+  {
+    questID: "5",
+    title: "Do Homework",
+    deadline: "3/31/2023",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
+    exp: 100,
+    pts: 50,
+  },
+  {
+    questID: "6",
+    title: "Do Homework",
+    deadline: "3/31/2023",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
+    exp: 100,
+    pts: 50,
+  },
+  {
+    questID: "7",
+    title: "Do Homework",
+    deadline: "3/31/2023",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
+    exp: 100,
+    pts: 50,
+  },
+  {
+    questID: "8",
+    title: "Do Homework",
+    deadline: "3/31/2023",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
+    exp: 100,
+    pts: 50,
+  },
+  {
+    questID: "9",
+    title: "Do Homework",
+    deadline: "3/31/2023",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
+    exp: 100,
+    pts: 50,
+  },
+  {
+    questID: "10",
+    title: "Do Homework",
+    deadline: "3/31/2023",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
+    exp: 100,
+    pts: 50,
+  },
+  {
+    questID: "11",
+    title: "Do Homework",
+    deadline: "3/31/2023",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
+    exp: 100,
+    pts: 50,
+  },
+  {
+    questID: "12",
+    title: "Do Homework",
+    deadline: "3/31/2023",
+    description: "Lorem cupidatat elit mollit esse quis ut excepteur aute nulla. Cillum voluptate duis esse minim amet voluptate nulla laborum ex voluptate do labore minim. Eu proident anim culpa veniam nulla ut culpa dolore pariatur pariatur est exercitation proident. Exercitation aute eiusmod culpa reprehenderit dolor aliqua nostrud id eiusmod ex. In ex id labore ex ut id eu excepteur incididunt duis eu consequat.",
+    exp: 100,
+    pts: 50,
   },
 ]
 
+
+
 export default function Guild() {
+  // const [readMore, setReadMore] = useState(false);
   return (
     <>
       <div className="page-container">
@@ -47,34 +135,69 @@ export default function Guild() {
 
           <div className='quest-page'>
             <h1 className='quest-title'>CSS Quest</h1>
-            <div className="task-list">
-              {
-                quest != null
-                  ?
-                  quest.map((task) => {
-                    return (
-                      <div className="task-content" key={task.questID}>
-                        <div className="task-holder">
-                          <p className='task-title'>{task.title} </p>
-                          <p className='task-des'>{task.deadline}</p>
-                          <p className='task-des'>{task.description}</p>
-                        </div>
-                        <div className="task-exp">
-                          <p>EXP:{task.exp}</p>
-                        </div>
-                        <div className="task-btn">
-                          {/* <GiTrashCan onClick={() => deleteTask(task.questID)} size={30} className='cls-btn' />
-                          <MdOutlineCheckCircleOutline onClick={() => finishtask(task.questID)} size={30} className='cls-btn' /> */}
-                          <GiTrashCan size={30} className='del-btn' />
-                          <MdOutlineCheckCircleOutline size={30} className='fin-btn' />
-                        </div>
-                      </div>
-                    )
-                  })
-                  :
-                  <h1>No Quest!</h1>
-              }
+
+            <div className="quest-page-container">
+              <div className="task-list">
+                {
+                  quest != null
+                    ?
+                    quest.map((task) => {
+                      return (
+                        <>
+                          <div className="task-card-holder">
+                            <div className="task-card" key={task.questID}>
+                              <input type="checkbox" id={task.questID} className='hide' />
+
+                              <div className="task-header">
+                                <p className='task-title'>{task.title} </p>
+                                <div className="task-container">
+                                  <div className="task-des-holder">
+                                    <p className='task-des'>+{task.exp} exp</p>
+                                    <p className='task-des'>+{task.pts} points</p>
+                                    <p className='task-des'>{task.deadline}</p>
+
+                                  </div>
+                                  <div className="task-exp">
+                                    <label htmlFor={task.questID} className='btn-drpdwn'><BsChevronExpand size={30} className='exp-btn' /></label>
+                                  </div>
+                                </div>
+
+                              </div>
+
+                              <div className="task-info">
+                                <p className='task-des'>{task.description}</p>
+                              </div>
+
+
+                            </div>
+
+                            <div className="task-btn">
+
+                            </div>
+
+                          </div>
+                        </>
+                      )
+                    })
+                    :
+                    <h1>No Quest!</h1>
+                }
+              </div>
+
+              <div className="submitted-act">
+                <h1 className='submitted-act-title'>Submitted Activities</h1>
+
+                {submittedact.map((task, index) => {
+                  return (
+                    <div className="sub-holder" key={index}>
+                        <p className="sub-title">{task.title}</p>
+                        <p className="sub-file">{task.file}</p>
+                    </div>
+                  )
+                })}
+              </div>
             </div>
+
           </div>
 
         </div>
