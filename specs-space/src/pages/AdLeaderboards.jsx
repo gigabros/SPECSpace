046fdf04315cfd80e0e1673916de207875da0d3a
JoshuaAyran <202011015@gordoncollege.edu.ph>
2023-04-30 21:49:23 +0800

    admin leaderboards fix

--- specs-space/src/pages/AdLeaderboards.jsx
+++ specs-space/src/pages/AdLeaderboards.jsx
@@ -2,6 +2,8 @@ import React, { useState,useEffect } from 'react'
 import './pagestyle.scss';
 import AdSidebar from '../components/AdSidebar'
 import axios from '../api/axios';
+import logo from '../components/specs_logo.png'
+import { BsAward } from 'react-icons/bs'
 
 const lboarddata = [
   {
@@ -118,27 +120,37 @@ export default function AdLeaderboards() {
 
             <div className="lboard-container">
 
+            <h1 className="lboard-title"><BsAward/> Ranking by Points <BsAward/></h1>
               <div className="lboard-content-holder">
-                <h1 className="lboard-title">Ranking by Points</h1>
                 {points.map((item, index) => {
                   return (
-                    <div className="lboard-content" key={index} id={item.top}>
-                      <p className='lboard-rank'>Rank {(index+1)} </p>
-                      <p className='lboard-username'>{item.name}</p>
-                      <p className='lboard-points'>{item.points} pts</p>
+                    <div className="lboard-content" key={item.stud_num} id={item.top}>
+                      <div className="lboard-dp-holder">
+                        <img src={logo} className='lboard-dp' />
+                      </div>
+                      <div className="lboard-label">
+                        <p className='lboard-rank'>RANK {(index + 1)}</p>
+                        <p className='lboard-username'>{item.name}</p>
+                        <p className='lboard-points'>{item.points} pts</p>
+                      </div>
                     </div>
                   )
                 })}
               </div>
 
+              <h1 className="lboard-title"><BsAward/> Ranking by Level <BsAward/></h1>
               <div className="lboard-content-holder">
-                <h1 className="lboard-title">Ranking by Level</h1>
                 {lvl.map((item, index) => {
                   return (
                     <div className="lboard-content" key={index} id={item.top}>
-                      <p className='lboard-rank'>Rank {(index+1)} </p>
+                    <div className="lboard-dp-holder">
+                      <img src={logo} className='lboard-dp' />
+                    </div>
+                    <div className="lboard-label">
+                      <p className='lboard-rank'>RANK {(index + 1)}</p>
                       <p className='lboard-username'>{item.name}</p>
-                      <p className='lboard-level'>level {item.lvl}</p>
+                      <p className='lboard-points'>Level {item.lvl}</p>
+                    </div>
                     </div>
                   )
                 })}
